60421bcdecf7bd25bb3437e62abda042cf5bd9c7
Jeff McNally <mcna0242@algonquinlive.com>
2016-10-20 15:30:54 -0400

    Lab3

--- app/src/main/java/com/jeffmcnally/lab1/ListItemsActivity.java
+++ app/src/main/java/com/jeffmcnally/lab1/ListItemsActivity.java
@@ -1,15 +1,24 @@
 package com.jeffmcnally.lab1;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.provider.MediaStore;
+import android.widget.Switch;
+import android.widget.Toast;
 
 
 public class ListItemsActivity extends AppCompatActivity {
@@ -23,6 +32,53 @@ public class ListItemsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Switch is On";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Switch is Off";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
+
+        CheckBox button2 = (CheckBox) findViewById(R.id.Button2);
+        button2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                        .setTitle(R.string.dialog_title)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK button
+                                Intent resultIntent = new Intent(  );
+                                resultIntent.putExtra("Response", "My information to share");
+                                setResult(Activity.RESULT_OK, resultIntent);
+                                finish();
+
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        })
+                        .show();
+            }
+        });
+
 
         ImageButton imgBtn = (ImageButton) findViewById(R.id.imageButton);
         imgBtn.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +87,9 @@ public class ListItemsActivity extends AppCompatActivity {
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
                 }
+
             }
         });
 
@@ -67,6 +125,16 @@ public class ListItemsActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageButton imgBtn = (ImageButton) findViewById(R.id.imageButton);
+            imgBtn.setImageBitmap(imageBitmap);
+        }
     }
 
 }
---
+++ app/src/main/java/com/jeffmcnally/lab1/StartActivity.java
@@ -0,0 +1,77 @@
+package com.jeffmcnally.lab1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.R.attr.data;
+
+public class StartActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "StartActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        setContentView(R.layout.activity_start);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+                startActivityForResult(intent, 5);
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 5){
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+        }
+        if (resultCode == Activity.RESULT_OK){
+            String messagePassed = data.getStringExtra("Response");
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, messagePassed, duration);
+            toast.show();
+        }
+    }
+}
