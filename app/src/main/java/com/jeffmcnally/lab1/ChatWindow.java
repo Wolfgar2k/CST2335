339da5012d3c4bcad9b5d796cba204c6825cc95f
Jeff McNally <mcna0242@algonquinlive.com>
2016-11-24 14:42:53 -0500

    Lab3

--- app/src/main/java/com/jeffmcnally/lab1/ChatWindow.java
+++ app/src/main/java/com/jeffmcnally/lab1/ChatWindow.java
@@ -1,16 +1,20 @@
 package com.jeffmcnally.lab1;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -20,6 +24,7 @@ public class ChatWindow extends AppCompatActivity {
     EditText chatText;
     ImageButton btnSend;
     ArrayList<String> chatAdapterList;
+    ChatAdapter chatAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +37,21 @@ public class ChatWindow extends AppCompatActivity {
         chatText = (EditText) findViewById(R.id.chatText);
         btnSend = (ImageButton) findViewById(R.id.btnSend);
         chatAdapterList = new ArrayList<String>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                chatAdapterList );
-
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                chatAdapterList );
+        chatAdapter = new ChatAdapter(this);
+        chatView.setAdapter(chatAdapter);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                      .setAction("Action", null).show();*/
+
                 chatAdapterList.add(chatText.getText().toString());
-                chatView.setAdapter(arrayAdapter);
+//                chatView.setAdapter(chatAdaptor);
+                chatAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/ getView()
+                chatText.setText("");
             }
         });
     }
@@ -81,6 +88,34 @@ public class ChatWindow extends AppCompatActivity {
         Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 
+    private class ChatAdapter extends ArrayAdapter<String>{
+        public ChatAdapter(Context ctx){
+            super(ctx, 0);
+        }
 
+        public int getCount(){
+            return chatAdapterList.size();
+        }
+
+        public String getItem(int position){
+            return chatAdapterList.get(position);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+
+            View result = null ;
+            if(position%2 == 0) {
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+            } else {
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+            }
+            TextView message = (TextView)result.findViewById(R.id.message_text);
+            message.setText(   getItem(position)  ); // get the string at position
+            return result;
+
+        }
+
+    }
 
 }
